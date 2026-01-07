23256e6d9a60554696e2104470dcac9064ddcbcd
giftin.id <calistanafisaa@gmail.com>
2026-01-08 03:04:28 +0700

    Update greeting text from 'Caca' to 'Cacaway'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hai, Caca'.split('')
+const text = 'Hai, Cacaway'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
