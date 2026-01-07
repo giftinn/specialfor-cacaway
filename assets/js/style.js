f18728c3df36447f74f0635d592fc160f7484387
giftin.id <calistanafisaa@gmail.com>
2026-01-08 03:12:26 +0700

    Update style.js

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Hai, selamat tanggal 8!').split('');
+        const titles = ('Selamat tanggal 8!').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Selamat tanggal 8 kita yang ke 10 yaa.       -Ridho Batman";
+            subtitle.innerText = "Selamat tanggal 8 kita yang ke 10 yaa. With love -Ridho Batman";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
